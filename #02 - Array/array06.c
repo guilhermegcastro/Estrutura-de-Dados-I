fc91572066fb5a31c027ff4d4743ca444c2cf10e
Guigos <guigasgdc@gmail.com>
2025-10-14 12:52:45 -0300

    Exercício de ARRAY corrigido.

--- #02 - Array/array06.c
+++ #02 - Array/array06.c
@@ -11,7 +11,6 @@
 #include <time.h>
 
 int main( ) {
-	srand(time(NULL));
 	
 	int N;
 	printf("Insire o tamanho da matriz: ");
@@ -19,32 +18,32 @@ int main( ) {
 
 	int matriz[N][N]; 
 	
-	for (int i = 0 ; i < N; i++){
+	for (int i = 0 ; i < N; i++){   //Define os valores dos espaços que não seja a diagonal.
 		for (int j = 0; j < N; j++) {
 			
 			matriz[i][j] = i + j;
 			if (i == j) 
 				matriz[i][j] = 0;
 		}
-		
 	}
 	
 	printf("\n");
 	
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < N; j++){
+	for (int i = 0; i < N; i++){ // Atribui o valor da diagonal.
+		for (int j = 0; j < N; j++){ 
 			if (i!=j)
-			matriz[i][i]+= 2* matriz[i][j];
-			printf(" %02d ", matriz[i][j]);
+			matriz[j][j]+= 2 * matriz[i][j];
 		}
-		printf("\n");
 	}
 	
-	
-	
-	
-	
-	
+	printf("|");
+	for (int i = 0; i < N; i++){ // Imprime a tabela.
+		for (int j = 0; j < N; j++){
+			printf(" %3d |", matriz[i][j]);
+		}
+		if(i!=N-1)
+		printf("\n|");
+	}
 	   return 0;
 	}
 	
